5c45826bc95f9a56125f0fdf26ca3fecb4c8cc60
Prosper <sirpros@gmail.com>
2021-10-30 14:55:36 +0100

    Testing for modules

--- db/migrate.go
+++ db/migrate.go
@@ -11,8 +11,8 @@ import (
 	"github.com/golang-migrate/migrate/database/mysql"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/mysql"
+	// "github.com/golang-migrate/migrate/v4"
+	// "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
