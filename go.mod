bf3218f0184f4f11bb25d5d2e0aea30abc268dc9
Prosper <sirpros@gmail.com>
2021-11-04 11:00:26 +0100

    Settup web server for first test

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/golang-migrate/migrate/v4 v4.15.1
+	github.com/gorilla/mux v1.8.0 // indirect
 )
--- oshop.go
+++ oshop.go
@@ -1,32 +1,56 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
 	// mig := migrate.Migrate()
 	// fmt.Println(mig)
 
-	/*
-		END POINT
-			#USER ENDPOINTS
-				/products
-					/add
-					/list
-					/edit
-					/delete
+	router := mux.NewRouter()
 
-				/purshases
-					/add
-					/list
-					/edit
-					/delete
+	router.HandleFunc("/", HomePage).Methods("GET")
 
-				/users
-					/add
-					/list
-					/edit
-					/delete
+	router.HandleFunc("/products", GetProducts).Methods("GET")
+	router.HandleFunc("/products", AddProducts).Methods("POST")
+	router.HandleFunc("/products", EditProducts).Methods("PUT")
+	router.HandleFunc("/products", DeleteProducts).Methods("DELETE")
 
-				/admin-dashboard
-				/public-dash
-	*/
+	router.HandleFunc("/users/", GetUsers).Methods("GET")
+	router.HandleFunc("/users/{userid:[0-9]+}", GetUser).Methods("GET")
+	router.HandleFunc("/users/{userid:[0-9]+}", UpdateUser).Methods("PUT")
+	router.HandleFunc("/users", CreateUser).Methods("POST")
+	router.HandleFunc("/users/{userid:[0-9]+}", DeleteUser).Methods("DELETE")
+
+	router.HandleFunc("/purchases", GetPurchases).Methods("GET") //admin
+	router.HandleFunc("/purchases/{userid:[0-9]+}", GetPurchases).Methods("GET")
+	router.HandleFunc("/purchase", CreatePurchase).Methods("POST")
+	router.HandleFunc("/purchase/{purchaseid:[0-9]+}", UpdatePurchase).Methods("PUT")
+	router.HandleFunc("/purchase/{purchaseid:[0-9]+}", DeletePurchase).Methods("DELETE")
+
+	http.ListenAndServe(":8080", router)
 }
 
+func HomePage(w http.ResponseWriter, r *http.Request){
+	msg := []byte("Welcome Oshop")
+	w.Write(msg)
+}
+
+func GetProducts(w http.ResponseWriter, r *http.Request)    {}
+func AddProducts(w http.ResponseWriter, r *http.Request)    {}
+func EditProducts(w http.ResponseWriter, r *http.Request)   {}
+func DeleteProducts(w http.ResponseWriter, r *http.Request) {}
+
+func GetUsers(w http.ResponseWriter, r *http.Request)   {}
+func GetUser(w http.ResponseWriter, r *http.Request)   {}
+func CreateUser(w http.ResponseWriter, r *http.Request) {}
+func UpdateUser(w http.ResponseWriter, r *http.Request) {}
+func DeleteUser(w http.ResponseWriter, r *http.Request) {}
+
+func GetPurchases(w http.ResponseWriter, r *http.Request)   {}
+func CreatePurchase(w http.ResponseWriter, r *http.Request) {}
+func UpdatePurchase(w http.ResponseWriter, r *http.Request) {}
+func DeletePurchase(w http.ResponseWriter, r *http.Request) {}
